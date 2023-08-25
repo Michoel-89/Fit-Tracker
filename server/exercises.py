2c5946452eaf8eaaa57cabde84b4f3017fb6d987
michoel <michoelr111@gmail.com>
2023-08-25 13:27:55 -0400

    create dict with different workouts

---
+++ server/exercises.py
@@ -0,0 +1,86 @@
+bodyweight_exercises = {
+    "Push-ups": {
+        "Description": "A classic upper body exercise that targets the chest, shoulders, and triceps.",
+        "Body Section": "Upper Body"
+    },
+    "Squats": {
+        "Description": "A lower body exercise that targets the quadriceps, hamstrings, and glutes.",
+        "Body Section": "Lower Body"
+    },
+    "Lunges": {
+        "Description": "Another lower body exercise that works the quadriceps, hamstrings, and glutes.",
+        "Body Section": "Lower Body"
+    },
+    "Plank": {
+        "Description": "A core exercise that helps strengthen the abdominal muscles and lower back.",
+        "Body Section": "Core"
+    },
+    "Crunches": {
+        "Description": "An abdominal exercise that focuses on the rectus abdominis muscles.",
+        "Body Section": "Core"
+    },
+    "Bridges": {
+        "Description": "An exercise that targets the glutes and lower back muscles.",
+        "Body Section": "Lower Body"
+    },
+    "Pull-ups": {
+        "Description": "An upper body exercise that primarily works the back and biceps.",
+        "Body Section": "Upper Body"
+    },
+    "Dips": {
+        "Description": "An exercise that targets the triceps and shoulders.",
+        "Body Section": "Upper Body"
+    },
+    "Burpees": {
+        "Description": "A full-body exercise that combines a squat, push-up, and jump.",
+        "Body Section": "Full Body"
+    },
+    "Mountain Climbers": {
+        "Description": "A cardiovascular exercise that also engages the core and leg muscles.",
+        "Body Section": "Cardio"
+    },
+    "Bodyweight Rows": {
+        "Description": "An exercise that works the upper back and biceps.",
+        "Body Section": "Upper Body"
+    },
+    "Step-ups": {
+        "Description": "A lower body exercise that targets the quadriceps and glutes.",
+        "Body Section": "Lower Body"
+    },
+    "Jumping Jacks": {
+        "Description": "A cardiovascular exercise that involves jumping while moving arms and legs.",
+        "Body Section": "Cardio"
+    },
+    "Wall Sits": {
+        "Description": "An isometric exercise that targets the quadriceps and engages the lower body.",
+        "Body Section": "Lower Body"
+    },
+    "Supermans": {
+        "Description": "An exercise that strengthens the lower back and posterior chain.",
+        "Body Section": "Lower Body"
+    },
+    "Russian Twists": {
+        "Description": "An exercise that engages the oblique muscles and improves core strength.",
+        "Body Section": "Core"
+    },
+    "High Knees": {
+        "Description": "A cardio exercise that involves lifting knees towards the chest alternately.",
+        "Body Section": "Cardio"
+    },
+    "Leg Raises": {
+        "Description": "An abdominal exercise that targets the lower abs and hip flexors.",
+        "Body Section": "Core"
+    },
+    "Handstand Push-ups": {
+        "Description": "An advanced exercise that strengthens shoulders and upper body.",
+        "Body Section": "Upper Body"
+    },
+    "Plank Jacks": {
+        "Description": "A variation of the plank exercise that adds a cardiovascular element.",
+        "Body Section": "Core"
+    },
+    "Box Jumps": {
+        "Description": "An explosive exercise that works on lower body power and coordination.",
+        "Body Section": "Lower Body"
+    }
+}
