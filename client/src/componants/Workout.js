6f0263f23fad6f3455e5e5ac79bda50e775efa92
michoel <michoelr111@gmail.com>
2023-08-31 09:48:39 -0400

    complete the workout componant

--- client/src/componants/Workout.js
+++ client/src/componants/Workout.js
@@ -2,5 +2,17 @@ import { useContext } from "react"
 import { Context } from "../App"
 function Workout() {
     const context = useContext(Context)
+    if (!context.workouts) {
+        return 
+    }
+    return <>
+    {context.workouts.map((workout) => {
+        return <div key={workout.id}>
+        <h2>{workout.name}</h2>
+        <h4>{workout.body_section}</h4>
+        <p>{workout.description}</p>
+        </div>
+    })}
+    </>
 }
 export default Workout
