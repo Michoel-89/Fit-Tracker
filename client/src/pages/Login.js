b33d269a4bbd5998d9684060e23552ac6348dea2
michoel <michoelr111@gmail.com>
2023-09-01 13:10:06 -0400

    complete signup and login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,6 +6,7 @@ import homeImg from "/home/michoel/post-grad-apps/fit-tracker/client/src/picture
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [signup, setSignup] = useState(false)
     const context = useContext(Context);
     // let navigate = useNavigate();
     const handleUsernameChange = (event) => {
@@ -18,16 +19,41 @@ function Login() {
     
       function handleSubmit(e) {
         e.preventDefault();
-        const userLogin = {
+        const user = {
           username: e.target.username.value,
           password: e.target.password.value,
         };
+        if (signup) {
+          fetch('/signup', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+          })
+          .then((r) => {
+            if (r.ok) {
+              return r.json();
+            } else {
+              throw new Error('Signup request failed');
+            }
+        })
+          .then((r) => {
+            setUsername('');
+            setPassword('');
+            context.setIsLoggedIn(true)
+        })
+          .catch((error) => {
+            console.error('Error occurred during signup:', error);
+            alert('Invalid input');
+        });
+        } else {
         fetch('/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify(userLogin),
+          body: JSON.stringify(user),
         })
           .then((r) => {
             if (r.ok) {
@@ -45,12 +71,67 @@ function Login() {
             console.error('Error occurred during login:', error);
             alert('Invalid input');
         });
+        }
       }
 
-      function handleSignupClick() {
-        // navigate('/signup');
+      function handleSignupLoginClick() {
+        setSignup(prev => !prev)
+        setUsername('');
+        setPassword('');
       }
-      console.log(homeImg)
+    if (signup) {
+      return (
+        <>
+          <img alt="Man doing pushups" src={homeImg} className="w-full max-h-screen object-center object-cover"/>
+          <div className="absolute top-36 left-1/2 transform -translate-x-1/2 mx-auto p-6 min-w-[30%] bg-gray-100 bg-opacity-75 rounded-lg shadow-md">
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <div>
+                  <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                    Username:
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={handleUsernameChange}
+                    className="mt-1 px-3 py-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                    Password:
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    className="mt-1 px-3 py-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Sign up
+                  </button>
+                </div>
+              </form>
+            <div className="mt-4 flex items-center justify-center">
+              <span className="text-sm text-gray-600">Already have an account?</span>
+              <button
+                onClick={handleSignupLoginClick}
+                className="ml-2 text-sm font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Login
+              </button>
+          </div>
+        </div>
+    </>
+      )
+    }
+    else {
     return (
     <>
       <img alt="Man doing pushups" src={homeImg} className="w-full max-h-screen object-center object-cover"/>
@@ -92,7 +173,7 @@ function Login() {
         <div className="mt-4 flex items-center justify-center">
           <span className="text-sm text-gray-600">Don't have an account?</span>
           <button
-            onClick={handleSignupClick}
+            onClick={handleSignupLoginClick}
             className="ml-2 text-sm font-medium text-indigo-600 hover:text-indigo-500"
           >
             Sign up
@@ -100,7 +181,8 @@ function Login() {
       </div>
     </div>
 </>
-  );
+  )
+}
 }
 
 export default Login
