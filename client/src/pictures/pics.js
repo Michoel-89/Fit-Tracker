0ac3820dca08bc9bd78a425f45b93805d6476be3
michoel <michoelr111@gmail.com>
2023-09-01 18:07:52 -0400

    add workout pics

---
+++ client/src/pictures/pics.js
@@ -0,0 +1,26 @@
+import pushups from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/man-working-out-2294361.jpg"
+import squats from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/man-squatting.jpg"
+import lunges from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/lunges.webp"
+import plank from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/plank.png"
+import crunches from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/crunches.jpg"
+import bridges from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/Bridges.webp"
+import pullUps from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/pullups.jpg"
+import dips from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/tricep dips.webp"
+import burpees from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/Burpee.webp"
+import mountainClimbers from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/Sporty-african-man-doing-mountain-climbers-ab-exercose.jpg"
+import bodyweightRows from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/Bodyweight-Rows-Pull-Up.png"
+import stepUps from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/StepUps.jpg"
+import jumpingJacks from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/jumping-jack.jpg"
+import wallSits from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/man-squatting.jpg"
+import supermans from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/superman.jpg"
+import russianTwists from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/Seated-Russian-Twist.webp"
+import highKnees from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/Fit-man-doing-high-knees-cardio-exercise.jpg"
+import legRaises from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/Muscular-Man-Working-Out-His-Lower-Abs-With-Lying-Leg-Raises-Exercise.webp"
+import handstandPushUps from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/Strong-Black-Man-Doing-Handstand-Push-Ups-Outdoors.jpg"
+import plankJacks from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/plank-jacks.png"
+import boxJumps from "/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/box-jump.jpg"
+
+let picsArray = [pushups, squats, lunges, plank, crunches, bridges, 
+    pullUps, dips, burpees, mountainClimbers, bodyweightRows, stepUps, jumpingJacks, wallSits, supermans, 
+    russianTwists, highKnees, legRaises, handstandPushUps, plankJacks, boxJumps]
+export default picsArray 
