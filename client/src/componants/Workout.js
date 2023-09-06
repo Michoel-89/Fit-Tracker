ada3e0f8f4c81fa06f12d8defc7bbe0ef2c8e80f
michoel <michoelr111@gmail.com>
2023-09-06 19:03:49 -0400

    add buttons for starting, canceling, completing workouts and submit form

--- client/src/componants/Workout.js
+++ client/src/componants/Workout.js
@@ -1,21 +1,62 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Context } from "../App"
 import picsArray from '/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/pics.js'
 function Workout() {
     const context = useContext(Context)
+    const [pending, setPending] = useState([])
+    const [pendingHover, setPendingHover] = useState(null)
+    const [revealWorkoutForm, setRevealWorkoutForm] = useState(null)
+    
     if (!context.workouts) {
         return 
     }
-    return <div class=" text-center grid grid-cols-3 gap-4">
+
+    function handleCompletedWorkout(e) {
+        e.preventDefault()
+        console.log('completed')
+    }
+
+    return <div className=" text-center grid grid-cols-3 gap-4">
         {context.workouts.map((workout, i) => {
         return (
         <div key={workout.id} className="border p-4">
-            <h2>{workout.name}</h2>
+            
+            {revealWorkoutForm === i ? 
+            <form onSubmit={(e) => handleCompletedWorkout(e)}>
+                <h2>Workout Details</h2>
+                <p>
+                  Workout Name: <strong>{workout.name}</strong>
+                </p>
+                <p>
+                  Body Section: <strong>{workout.body_section}</strong>
+                </p>
+                <h2>Workout Count</h2>
+                <input
+                  type="number"
+                  placeholder="Enter workout count"
+                  required
+                />
+                <button className="border" type="submit">Submit</button>
+            </form>
+            : 
+            <><h2>{workout.name}</h2>
             <div className="aspect-w-1 aspect-h-2 mb-4">
                 <img className="w-full h-80 object-cover" src={picsArray[i]} alt={workout.name}/>
             </div>
             <h4>{workout.body_section}</h4>
-            <p>{workout.description}</p>
+            {!pending.includes(i) ?
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setPending(prev => [...prev, i])}>Start Workout</button>
+            :
+            <div> 
+                {pendingHover === i ?
+                <div onMouseLeave={() => setPendingHover(null)}><button className="bg-red-500 hover:bg-red-700 font-bold mr-1 text-white py-2 px-4 rounded" onClick={() => setPending(prev => prev.filter((num) => num !== i))}>Cancel</button>
+                <button className=" ml-1 hover:bg-green-700 bg-green-500 font-bold text-white py-2 px-4 rounded" onClick={() => setRevealWorkoutForm(i)}>Complete</button></div>
+                :
+                <button className="bg-orange-500 text-white font-bold py-2 px-4 rounded" onMouseEnter={() => setPendingHover(i)}>Workout in Progress</button>
+                }
+            </div>
+            }   
+            <p>{workout.description}</p></>}
         </div>
         )
     })}
