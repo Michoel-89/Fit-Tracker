11f96e95c64e7430130792c13f40a43e5497c9d4
michoel <michoelr111@gmail.com>
2023-09-06 19:00:56 -0400

    style both navbars

--- client/src/componants/LoggedInNavbar.js
+++ client/src/componants/LoggedInNavbar.js
@@ -11,13 +11,13 @@ function LoggedInNavbar() {
         })
     }
     return <>
-<div class="flex bg-black p-4 justify-between items-center">
-    <h2 class="text-white font-bold text-xl">Fit Tracker</h2>
-    <div class="space-x-4">
-        <button class="text-white px-4 py-2 rounded">
+<div className="flex bg-black p-4 justify-between items-center">
+    <h2 className="text-white font-bold text-xl">Fit Tracker</h2>
+    <div className="space-x-4">
+        <button className="text-white px-4 py-2 rounded">
             <Link to="/MyWorkouts">My Workouts</Link>
         </button>
-        <button class="text-white px-4 py-2 rounded" onClick={handleLogoutClick}>
+        <button className="text-white px-4 py-2 rounded" onClick={handleLogoutClick}>
             <Link to="/">Logout</Link>
         </button>
     </div>
--- client/src/componants/Navbar.js
+++ client/src/componants/Navbar.js
@@ -1,10 +1,10 @@
 
 function Navbar() {
     return <>
-<div class="fixed top-0 left-0 right-0 bg-black bg-opacity-20 p-4 flex justify-between items-center z-50">
-    <h2 class="text-white font-bold text-xl">Fit Tracker</h2>
-    <div class="space-x-4"> 
-        <button class="px-4 py-2 rounded">Navbar Equalizer</button>
+<div className="fixed top-0 left-0 right-0 bg-black bg-opacity-20 p-4 flex justify-between items-center z-50">
+    <h2 className="text-white font-bold text-xl">Fit Tracker</h2>
+    <div className="space-x-4"> 
+        <button className="px-4 py-2 rounded opacity-0">.</button>
     </div>
 </div>
 </>
