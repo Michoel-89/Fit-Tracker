8b9c989c3f9e5ed1db4f9e845c9a41fd36710cac
michoel <michoelr111@gmail.com>
2023-09-06 18:58:51 -0400

    display users workout count

--- client/src/pages/MyWorkout.js
+++ client/src/pages/MyWorkout.js
@@ -1,6 +1,10 @@
+import { useContext } from "react"
+import { Context } from "../App"
 function MyWorkout() {
+    const context = useContext(Context)
     return <>
-    <h2>MyWorkout</h2>
+        {context.myWorkouts.map((workout) => {
+            return <h2 key={workout.id}>{workout.workout_count}</h2>})}
     </>
 }
 export default MyWorkout
