fc26451255f2308d93a61a9e0f6f57266c8e38e0
michoel <michoelr111@gmail.com>
2023-09-15 13:27:08 -0400

    websockets work for fast messaging!!

--- client/src/componants/Message.js
+++ client/src/componants/Message.js
@@ -1,6 +1,8 @@
-import { useContext, useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import { Context } from "../App"
 import messageIcon from '/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/messageIcon.jpg'
+import io from 'socket.io-client';
+const socket = io('http://localhost:5555');
 
 function reformatTimestamp(timestamp) {
   const dateParts = timestamp.split(' ');
@@ -23,31 +25,30 @@ function reformatTimestamp(timestamp) {
 
 function Message() {
     const [revealMsgs, setRevealMsgs] = useState(false)
-    const [message, setMessage] = useState('')
+    const [messageInState, setMessage] = useState('')
     const context = useContext(Context)
 
-    function handleSendMessage() {
-        if (message.length < 1) {
-          return null
-        }
-        const newMessage = {
-          text: message,
-          user_id: context.user.id
-        }
-        fetch('messages', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(newMessage)
-        })
-        .then(r => r.json())
-        .then(r => {
-          const newList = [...context.messages, r]
-          context.setMessages(newList)
-          setMessage('')
-        })
+    useEffect(() => {
+      socket.on('message', (newMessage) => {
+          if (Array.isArray(context.messages)) {
+            context.setMessages([...context.messages, newMessage]);
+          }
+      })
+  }, [context]);
+
+  function handleSendMessage() {
+    if (messageInState.length < 1) {
+        return null;
     }
+    const newMessage = {
+        text: messageInState,
+        user_id: context.user.id
+    };
+
+    socket.emit('message', newMessage);
+
+    setMessage('');
+}
 
     if (!context.messages) {
         return 
@@ -57,15 +58,15 @@ function Message() {
           {revealMsgs ? (
             <div className="bg-white p-4 rounded-lg shadow-lg">
             <h1 className="text-3xl font-bold mb-4">Fit Community</h1>
-            <div className="max-h-[60vh] overflow-y-auto">
-            {context.messages.map((message) => {
-                return <div key={message.id} className={message.user.id === context.user.id ? 'bg-green-300 p-3 mb-4 rounded-lg shadow-md ml-10' :'bg-gray-100 p-3 mb-4 rounded-lg shadow-md mr-10'}>
+            <div className="max-h-[60vh] max-w-[60vh] overflow-y-auto">
+            {context.messages.map((msg) => {
+                return <div key={msg.id} className={msg.user.id === context.user.id ? 'bg-green-300 p-3 mb-4 rounded-lg shadow-md ml-10' :'bg-gray-100 p-3 mb-4 rounded-lg shadow-md mr-10'}>
                 <div className='flex items-right justify-between'>
                     <div>
-                      <h2 className="font-semibold text-lg">{message.user.id === context.user.id ? 'Me' : message.user.username}:</h2>
-                      <p className="text-gray-600">{message.text}</p>
+                      <h2 className="font-semibold text-lg">{msg.user.id === context.user.id ? 'Me' : msg.user.username}:</h2>
+                      <p className="text-gray-600">{msg.text}</p>
                       </div>
-                      <p className="text-sm mb-6 text-gray-500">{reformatTimestamp(message.time_created)}</p>
+                      <p className="text-sm mb-6 text-gray-500">{reformatTimestamp(msg.time_created)}</p>
                     </div>
                 </div>
             })}
@@ -73,7 +74,7 @@ function Message() {
                 <input
                 placeholder="Type your message..."
                 className="w-full p-2 border rounded"
-                value={message}
+                value={messageInState}
                 onChange={(e) => setMessage(e.target.value)}
                 />
             </div>
@@ -83,7 +84,12 @@ function Message() {
             </div>
             <div className="flex justify-end mt-2">
             <button
-                onClick={() => setRevealMsgs(false)}
+                onClick={() => {
+                  setRevealMsgs(false)
+                  if (socket) {
+                    socket.close();
+                  }
+                }}
                 className="text-blue-500 hover:text-blue-700 font-semibold"
             >
                 Close
