18a5b03d60646aaf0f31be93f944b8de28814327
michoel <michoelr111@gmail.com>
2023-09-21 13:21:32 -0400

    bar chart completed

---
+++ client/src/componants/Chart.js
@@ -0,0 +1,44 @@
+import React, { PureComponent } from 'react';
+import { useContext } from 'react';
+import { Context } from '../App';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, BarChartainer } from 'recharts';
+
+
+function Chart() {
+  const context = useContext(Context)
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="bg-white bg-opacity-90 p-2 border border-gray-300">
+          <p>{`Date: ${label}`}</p>
+          <p>{`${payload[0].value} ${payload[1].value}`}</p>
+        </div>
+      );
+    }
+  
+    return null;
+  };
+  
+    if (!context.myWorkouts) {
+      return <h2>Loading...</h2>
+    }
+    
+    return (<>
+        <ResponsiveContainer height={400}>
+        <BarChart
+          width={400}
+          height={400}
+          data={context.myWorkouts}
+        >
+          <XAxis hide dataKey="time_created" />
+          <YAxis />
+          <Tooltip cursor={{fill: 'transparent'}} content={CustomTooltip}/>
+          <Bar type="monotone" dataKey='workout_count' isAnimationActive={false} stroke="#8884d8" fill="rgb(0,250,154)" />
+          <Bar type="monotone" dataKey='workout.name' stroke="#8884d8" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+      </>
+    );
+  }
+export default Chart
