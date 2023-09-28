62107a6b33e011ebd025d21e154ccf7efc153db4
michoel <michoelr111@gmail.com>
2023-09-27 20:07:02 -0400

    change url for deployment

--- client/src/componants/Message.js
+++ client/src/componants/Message.js
@@ -3,7 +3,7 @@ import { Context } from "../App"
 import messageIcon from '/home/michoel/post-grad-apps/fit-tracker/client/src/pictures/messageIcon.jpg'
 import io from 'socket.io-client';
 import { reformatTimestamp } from '../functions/functions';
-const socket = io('http://127.0.0.1:5555')
+const socket = io('https://fit-tracker-g614.onrender.com/')
 
 function Message() {
     const [revealMsgs, setRevealMsgs] = useState(false)
